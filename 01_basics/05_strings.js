bb53f747ca8ee229fa23c5fe701c8c3d43b57900
Soumo Sarkar <120677951+SuperSection@users.noreply.github.com>
2023-09-04 15:28:58 +0000

    string & some of its methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = "Pagol"
+const repoCount = 5
+
+// string interpolation
+// console.log(`I am Paglir ${name}, my repo count is ${repoCount}`);
+
+/* const codeName = new String("super-section")
+console.log(codeName[0])
+console.log(codeName.__proto__);
+
+console.log(codeName.length);
+console.log(codeName.toUpperCase());
+
+console.log(codeName.indexOf('t'));
+console.log(codeName.charAt(9));
+
+const subString = codeName.substring(0, 5)
+console.log(subString);
+
+const slicedstring= codeName.slice(-7, 13);
+console.log(slicedstring); */
+
+const userName = "   soumo   "
+console.log(userName);
+console.log(userName.trim());
+
+const url = "http://supersection.com/soumo%20sarkar"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('pagol'));
+console.log(url.split('/'));
