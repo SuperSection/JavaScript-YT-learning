5e981aa13d43df09dd319a6849545bcf6f34ff88
Soumo Sarkar <120677951+SuperSection@users.noreply.github.com>
2023-09-04 03:06:39 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // "alert is not defined" error because we are using Nodejs, not browser
+
+// code readability should be high
+
+console.log("Soumo")
+
+let name = "Pagol"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+/*  PREMITIVE DataTypes
+number => 2 to the power 53
+bigint
+string => " "
+boolean => true / false
+null => standalone value
+undefined => no value is assinged
+symbol => unique
+*/
+
+// object
+
+console.log(typeof null)        // object
+console.log(typeof undefined)   // undefined
