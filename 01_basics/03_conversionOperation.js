e6cc691503496189a772c94dcab310d1623dd029
Soumo Sarkar <120677951+SuperSection@users.noreply.github.com>
2023-09-04 11:46:13 +0000

    conversion of DataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,20 +29,62 @@ console.log(undfInNumber) */
 
 /* Conversion to Boolean */
 
-let isLoggedIn = 1;
+/* let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
-let testString = "Pagol";
+let testString = "Paglir Pagol";
 let BooleanTestString = Boolean(testString)
-console.log(BooleanTestString)
+console.log(testString, BooleanTestString)
 // empty string ( "" [not " "] ) => false
 // "pagol" => true
-
+*/
 
 /* Conversion to String */
 
-let someNumber = 55
+/* let someNumber = 55
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
-console.log(stringNumber)
+console.log(stringNumber) */
+
+
+/* ------------- OPERATIONS ------------- */
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.table([2+2, 2-2, 2*3, 2**3, 2/3, 2%3]);
+
+let str1 = " love you "
+let str2 = "Pagli"
+
+let str = "I" + str1 + str2
+// console.log(str);
+
+/* console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3"); */
+
+// console.log(3+4*5%3);
+
+/* console.log(+true);
+console.log(+""); */
+
+/* let num1, num2, num3
+num1 = num2 = num3 = 2+2 */ // never recommended ths type of coding practice
+
+// `` -> template literals
+
+/* let x = 3;
+const y = x++;  // first stored then increamented
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;  // first increamented then stored
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4" */
