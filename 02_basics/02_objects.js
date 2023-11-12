ea8bea20ad0f23a34ca1924a72377e532935837c
Soumo Sarkar <120677951+SuperSection@users.noreply.github.com>
2023-11-12 11:07:34 +0000

    [fix]/define objects within {...}:curly braces

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -69,7 +69,7 @@ const obj1 = { 1: "a", 2: "b" };
 const obj2 = { 3: "c", 4: "d" };
 const obj3 = { 5: "e", 6: "f" };
 
-const obj4 = (obj1, obj2)
+const obj4 = { obj1, obj2 };
 console.log(obj4);
 
 const obj5 = Object.assign(obj1, obj2); // all the values are getting stored in the first object
