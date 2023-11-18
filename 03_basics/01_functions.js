fb42598e5d91fccd35b413cf3a09fa4bd50c1997
Soumo Sarkar <120677951+SuperSection@users.noreply.github.com>
2023-11-18 16:02:25 +0000

    Functions with objects and arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,7 +7,7 @@ function sayMyName() {
 }
 
 sayMyName; // reference of the function
-sayMyName(); //execution of the function
+// sayMyName(); //execution of the function
 
 /*
 function addTwoNumbers(number1, number2){ // parameters
@@ -28,7 +28,7 @@ function addTwoNumbers(number1, number2) {
 }
 
 const result = addTwoNumbers(5, "4"); // arguments
-console.log("Result:", result);
+// console.log("Result:", result);
 
 function loginUserMessage(username = "supersection") {
   // this block will never run because the parameter has a default value
@@ -44,6 +44,37 @@ function loginUserMessage(username = "supersection") {
   return `${username} just logged in`;
 }
 
-console.log(loginUserMessage("Soumo"));
-console.log(loginUserMessage(""));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("Soumo"));
+// console.log(loginUserMessage(""));
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num){ // rest operator as function parameter
+  return num;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 1000))
+
+const product = {
+  items: "food", // while passing an object into a function, must check type and the property exists or not
+  price: 299,
+}
+
+function handleObject(anyObject) {
+  console.log(`Item is a ${anyObject.item} and price is ${anyObject.price}`);
+}
+
+handleObject(product)
+handleObject({
+  item: "phone",
+  price: 8999
+})
+
+const myNewArray = [200, 500, 800, 7000]
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([8, 7, 12, 10, 17, 29]));
